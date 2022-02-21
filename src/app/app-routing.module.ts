90358a0c28629b955a34cfe460197917618a0404
Nikunj1896 <Nikunj.projects9893@gmail.com>
2022-02-22 00:20:49 +0530

    created routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", redirectTo: "", },
+  {
+    path: "",
+    pathMatch: "full",
+    component: LandingPageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
